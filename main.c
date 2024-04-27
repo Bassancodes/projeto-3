938eddf92f0ef46d34dba720cad0781c6a894d23
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-04-27 02:12:10 +0000

    criando a main

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include "funcoes.h"
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main() {
+    int opcao;
+
+    do {
+        printf("\nSelecione uma opção:\n");
+        printf("1. Adicionar contato\n");
+        printf("2. Listar contatos\n");
+        printf("3. Deletar contato\n");
+        printf("4. Salvar agenda\n");
+        printf("5. Carregar agenda\n");
+        printf("0. Sair\n");
+        printf("Opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                adicionarContato();
+                break;
+            case 2:
+                listarContatos();
+                break;
+            case 3:
+                deletarContato();
+                break;
+            case 4:
+                salvarAgenda();
+                break;
+            case 5:
+                carregarAgenda();
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (opcao != 0);
+
+    return 0;
 }
