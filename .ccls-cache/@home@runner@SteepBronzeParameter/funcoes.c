1719b0a1b25202b91ca49be3ccf1bc31de0f5b01
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-05-24 21:30:54 +0000

    comentando

--- .ccls-cache/@home@runner@SteepBronzeParameter/funcoes.c
+++ .ccls-cache/@home@runner@SteepBronzeParameter/funcoes.c
@@ -6,32 +6,28 @@
 
 int num_contatos = 0; // Inicializa o número de contatos como zero.
 
-void adicionarContato() {
-    if (num_contatos >= MAX_CONTATOS) {
-        printf("A agenda está cheia!\n");
-        return;
+void adicionarContato() { // Função para adicionar um novo contato.
+    if (num_contatos >= MAX_CONTATOS) { // Verifica se o número máximo de contatos foi atingido.
+        printf("A agenda está cheia!\n"); // Informa ao usuário que a agenda está cheia.
+        return; // Sai da função sem adicionar um novo contato.
     }
 
-    Contato novoContato;
+    Contato novoContato; // Declara uma variável para armazenar o novo contato.
 
-    printf("Digite o nome: ");
-    scanf("%s", novoContato.nome);
-    printf("Digite o sobrenome: ");
-    scanf("%s", novoContato.sobrenome);
-    printf("Digite o email: ");
-    scanf("%s", novoContato.email);
-    printf("Digite o telefone: ");
-    scanf("%s", novoContato.telefone);
+    printf("Digite o nome: "); // Solicita ao usuário que digite o nome do contato.
+    scanf("%s", novoContato.nome); // Lê o nome digitado pelo usuário e armazena na variável 'novoContato'.
 
-    agenda[num_contatos++] = novoContato;
-    printf("Contato adicionado com sucesso!\n");
-}
+    printf("Digite o sobrenome: "); // Solicita ao usuário que digite o sobrenome do contato.
+    scanf("%s", novoContato.sobrenome); // Lê o sobrenome digitado pelo usuário e armazena na variável 'novoContato'.
 
-void listarContatos() {
-    printf("Lista de contatos:\n");
-    for (int i = 0; i < num_contatos; i++) {
-        printf("%d: %s %s - %s, %s\n", i + 1, agenda[i].nome, agenda[i].sobrenome, agenda[i].email, agenda[i].telefone);
-    }
+    printf("Digite o email: "); // Solicita ao usuário que digite o email do contato.
+    scanf("%s", novoContato.email); // Lê o email digitado pelo usuário e armazena na variável 'novoContato'.
+
+    printf("Digite o telefone: "); // Solicita ao usuário que digite o telefone do contato.
+    scanf("%s", novoContato.telefone); // Lê o telefone digitado pelo usuário e armazena na variável 'novoContato'.
+
+    agenda[num_contatos++] = novoContato; // Adiciona o novo contato à agenda e incrementa o contador de contatos.
+    printf("Contato adicionado com sucesso!\n"); // Informa ao usuário que o contato foi adicionado com sucesso.
 }
 
 
